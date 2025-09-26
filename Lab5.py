a5ab84e2b237b3cd65ad7e275abb4fb05ee0d16d
Duc Minh Nguyen <2476537@edu.vaniercollege.qc.ca>
2025-09-26 16:39:31 -0400

    first two questions finished

--- Lab5.py
+++ Lab5.py
@@ -5,14 +5,35 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    square = ""
+    x = 1 
+
+    while x <= n:
+        if x == 1 or x == n:
+            square += ("*" * n + "\n")
+        else:
+            square += ("*" + (" " * (n - 2)) + "*" + "\n")
+        x += 1
+
+    return (square.strip())
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    pattern = ""
+    x = 1
+    while x <= n:
+        i = 1
+        while i <= x:
+            pattern += str(i)
+            i += 1
+        pattern += "\n"
+        x += 1
+        
+        
+    return (pattern.strip())
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
@@ -25,3 +46,5 @@ def sum_of_natural_numbers(n):
 # *******
 def centered_star_pyramid(n):
     return ""
+
+
